cbd8ebec7a399ec9335752b78d83f7e8631088b2
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-08 13:48:44 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -24,7 +24,8 @@ end
      input = gets.strip
      index = input_to_index(input)
      if valid_move?(board,index)
-       move(board,index) 
+       move(board,index)
+       display_board(board) 
      end
   end
 
