6a323f2fb28d8830443c26afcad6feb93d91c446
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-08 13:46:23 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -20,15 +20,8 @@ def valid_move?(board, index)
 end
 
   def turn(board)
-  puts "Please enter 1-9:"
-  input = gets.strip
-  index = input_to_index(input)
-    if valid_move?(board, index)
-      move(board,index)
-       display_board(board)
-     else
-       turn(board)
-    end
+     puts "Please enter 1-9:"
+
   end
 
 
