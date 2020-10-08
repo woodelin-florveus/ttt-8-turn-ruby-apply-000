d5379f43f9638102553177ee9bbe09c648699ad5
Woodelin Florveus <woodelinflorveus@gmail.com>
2020-10-08 13:48:28 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -23,7 +23,8 @@ end
      puts "Please enter 1-9:"
      input = gets.strip
      index = input_to_index(input)
-     if valid_move?(board,index) 
+     if valid_move?(board,index)
+       move(board,index) 
      end
   end
 
